9fce9eeae62ff52b083d28ae1e771e7e5f53b763
Etornam Sunu Bright <sunumacbright@gmail.com>
2020-01-05 21:05:45 +0000

    Create single_chat_page.dart

---
+++ lib/single_chat_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SingleChatPage extends StatelessWidget {
+  final chatData;
+
+  const SingleChatPage({Key key, @required this.chatData}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
